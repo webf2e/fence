a8ad604385e1288f49add9f1462a1482678cefe5
liuwenbin <webf2e@163.com>
2019-12-07 14:18:29 +0800

    提交余额判断报警

--- route/IndexRoute.py
+++ route/IndexRoute.py
@@ -8,7 +8,12 @@ indexRoute = Blueprint('indexRoute', __name__)
 def current():
     return Response(FenceService.getLast(), mimetype='application/json')
 
-@indexRoute.route('/getFences',methods=["POST"])
-def getFences():
+@indexRoute.route('/getInOutFences',methods=["POST"])
+def getInOutFences():
+    count = request.form.get("count")
+    return Response(FenceService.getFences(count), mimetype='application/json')
+
+@indexRoute.route('/getHisFences',methods=["POST"])
+def getHisFences():
     count = request.form.get("count")
     return Response(FenceService.getFences(count), mimetype='application/json')
